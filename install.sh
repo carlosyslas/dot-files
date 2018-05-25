660754f7271edf49914b319ba74db62bd9fd98f8
Carlos Yslas <carlos.yslasa@gmail.com>
2018-05-25 14:00:26 +0200

    Initial commit

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+INSTALL_DIR=~/.dot-files
+
+# Clone or update
+if [ -d $INSTALL_DIR ]
+then
+    (cd $INSTALL_DIR ; git pull)
+else
+    git clone git@github.com:carlosyslas/dot-files.git $INSTALL_DIR
+fi
+
+# Setup bash
+if ! grep -q "source $INSTALL_DIR/bash/main.sh" ~/.bashrc
+then
+    echo "source $INSTALL_DIR/bash/main.sh" >> ~/.bashrc
+fi
+
+#ln -s $INSTALL_DIR/bash/
