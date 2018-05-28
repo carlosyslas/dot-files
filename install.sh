c551ee175184e1453529a2a7c8a8f9474e5a7c11
Carlos Yslas <carlos.yslasa@gmail.com>
2018-05-28 16:10:23 +0200

    Add tmux conf

--- install.sh
+++ install.sh
@@ -3,17 +3,20 @@
 INSTALL_DIR=~/.dot-files
 
 # Clone or update
-if [ -d $INSTALL_DIR ]
-then
+if [ -d $INSTALL_DIR ]; then
     (cd $INSTALL_DIR ; git pull)
 else
     git clone git@github.com:carlosyslas/dot-files.git $INSTALL_DIR
 fi
 
 # Setup bash
-if ! grep -q "source $INSTALL_DIR/bash/main.sh" ~/.bashrc
-then
+if ! grep -q "source $INSTALL_DIR/bash/main.sh" ~/.bashrc; then
     echo "source $INSTALL_DIR/bash/main.sh" >> ~/.bashrc
+fi
+
+# Tmux
+if [ ! -f ~/.tmux.conf ]; then
+    ln -s $INSTALL_DIR/tmux/tmux.conf ~/.tmux.conf
 fi
 
 #ln -s $INSTALL_DIR/bash/
