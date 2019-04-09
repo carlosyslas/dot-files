cac3359f812b5f308191cdf9679aef303c5fa7e5
Carlos Yslas <cyslas@newstore.com>
2019-04-09 11:34:42 +0200

    Remove junk.

--- iterm/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ITERM="iTerm"
-
-if [ ! -d "/Applications/$ITERM.app" ]; then
-    self__banner "Installing $ITERM"
-    self__install_zip_app https://iterm2.com/downloads/stable/latest "$ITERM"
-fi
--- vscode/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-VSCODE="Visual Studio Code"
-
-if [ ! -d "/Applications/$VSCODE.app" ]; then
-    self__banner "Installing $VSCODE"
-    self__install_zip_app https://go.microsoft.com/fwlink/?LinkID=620882 "$VSCODE"
-fi
