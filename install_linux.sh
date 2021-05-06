08228388467e918c88f95ad6e31a8e5014dd80b2
Carlos Yslas <carlos.yslasa@gmail.com>
2021-05-06 19:54:31 +0000

    Add ranger.

--- install_linux.sh
+++ install_linux.sh
@@ -1,5 +1,5 @@
 sudo apt-get update -y
-sudo apt-get install -y stow neovim jq fzf bat exa ripgrep
+sudo apt-get install -y stow neovim jq fzf bat exa ripgrep ranger
 
 mkdir -p ~/.local/bin
 ln -s /usr/bin/batcat ~/.local/bin/bat
