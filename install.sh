8479626a2a9b4da563e85737c613d86e3230b036
Carlos Yslas <carlos.yslasa@gmail.com>
2025-06-05 19:18:44 -0600

    Add simple install script

--- install.sh
+++ install.sh
@@ -1,28 +1,5 @@
-#!/bin/bash
-mkdir -p ~/src
-git clone git@github.com:carlosyslas/dot-files.git ~/src/dot-files
-cd ~/src/dot-files/
+#!/usr/bin/env bash
 
-case "$(uname -s)" in
-	Linux*) source install_linux.sh ;;
-	Darwin*) source install_mac.sh ;;
-esac
-
-# Install fnm
-curl -fsSL https://fnm.vercel.app/install | bash
-source /home/carlos/.zshrc
-
-# Install neovim
-source setup_neovim.sh
-
-# Install tmux
-stow -t $HOME tmux
-
-# Install fish
-#fish setup_fish.fish
-
-# Install alacritty
-stow -t $HOME alacritty
-
-# Install emacs
-stow -t $HOME emacs
+sudo dnf update
+sudo dnf upgrade -y
+sudo dnf install -y vim stow alacritty wofi wlogout
