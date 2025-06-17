c6d134c1d810579289bd710d785da9b9be58b5ed
Carlos Yslas <carlos.yslasa@gmail.com>
2025-06-16 19:31:15 -0600

    More cleanup

--- Makefile
+++
@@ -1,5 +0,0 @@
-DEPENDENCIES=zsh vim tmux ranger jq httpie
-
-install-dependencies:
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	brew install $(DEPENDENCIES)
--- setup_fish.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/fish
-stow -t $HOME fish
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-
-fisher install jethrokuan/z
-fisher install dracula/fish
-fisher install simnalamburt/shellder
