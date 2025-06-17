8a6ebdb0ee1abc00465daed4466b83bd51076e62
Carlos Yslas <carlos.yslasa@gmail.com>
2025-06-16 18:44:42 -0600

    Add basic mpd config

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf update
 sudo dnf upgrade -y
-sudo dnf install -y vim stow alacritty sway swaylock waybar wofi wlogout git
+sudo dnf install -y vim neovim stow alacritty sway swaylock waybar wofi wlogout git mpd pcmanfm
