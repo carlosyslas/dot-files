cfcdd75c37338838fa20b60151c70e220962b02f
Carlos Yslas <carlos.yslasa@gmail.com>
2026-02-09 23:01:52 -0600

    Add flathub

--- install.sh
+++ install.sh
@@ -6,8 +6,11 @@ sudo dnf update
 sudo dnf upgrade -y
 sudo dnf install -y vim stow alacritty git timeshift emacs yt-dlp imv mpv vlc zsh fastfetch
 sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install -y com.brave.Browser app.zen_browser.zen io.gitlab.theevilskeleton.Upscaler org.upscayl.Upscayl io.github.kolunmi.Bazaar org.qbittorrent.qBittorrent com.rafaelmardojai.Blanket com.github.johnfactotum.Foliate
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv zsh)"
 brew install anomalyco/tap/opencode
 brew install starship
